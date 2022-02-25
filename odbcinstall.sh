181bbdba8f4d603e1f00a46786c78138bf0ff7c9
Ryan Douglass <ryan@rydoug.com>
2022-02-24 20:26:06 -0500

    Update odbcinstall.sh

--- odbcinstall.sh
+++ odbcinstall.sh
@@ -1,6 +1,3 @@
-apt-get update
-apt-get -y install curl
-
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
